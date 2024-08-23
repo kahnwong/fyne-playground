a0a8966d55e7b00a838a33d4d6bd5dfbcfefdb0f
Karn Wong <karn@karnwong.me>
2024-08-23 16:25:23 +0700

    extract to tab_meetings.go

---
+++ tab_meetings.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func meetings() *fyne.Container {
+	// constant
+	blankLine := canvas.NewText("", color.White)
+
+	// header
+	heading := canvas.NewText("Meeting Cost Calculator", color.White)
+	heading.TextSize = 30
+
+	subheading := canvas.NewText("How much does this meeting really cost?", color.White)
+	subheading.TextSize = 25
+
+	content := container.New(layout.NewVBoxLayout(), heading, blankLine, subheading, blankLine)
+
+	// meetingsSection
+	// -- output -- //
+	output := container.New(layout.NewVBoxLayout(), widget.NewLabel("$200"), widget.NewLabel("$20"), widget.NewLabel("$120"))
+
+	// -- form -- //
+	str := binding.NewString()
+	err := str.Set("Hi!")
+	if err != nil {
+		return nil
+	}
+
+	form := container.NewVBox(
+		widget.NewLabelWithData(str),
+		widget.NewEntryWithData(str),
+	)
+
+	meetingsSection := container.New(layout.NewHBoxLayout(), output, blankLine, form)
+
+	return container.New(layout.NewVBoxLayout(), content, meetingsSection)
+}
