f385d9fc9973184e67344d6a61d1cfcb75f1fe4b
Karn Wong <karn@karnwong.me>
2024-08-23 15:37:08 +0700

    add icon

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+	w := a.NewWindow("Larger")
+	w.SetContent(widget.NewLabel("More content"))
+	w.Resize(fyne.NewSize(800, 800))
+	w.Show()
+
+	a.Run()
 }
